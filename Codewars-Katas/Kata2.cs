8a2ce64f618b5c7691aabaab3c45146f65fafe66
Roger <morenoroger001@gmail.com>
2020-12-22 23:51:49 -0500

    feat: Add Kata 2 to solution

---
+++ Codewars-Katas/Kata2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars_Katas
+{
+    class Kata2
+    {
+        public static int sumTwoSmallestNumbers(int[] numbers)
+        {
+            int minor = numbers.Min();
+            numbers[Array.IndexOf(numbers, minor)] = numbers.Max();
+
+            return minor + numbers.Min();
+        }
+    }
+}
